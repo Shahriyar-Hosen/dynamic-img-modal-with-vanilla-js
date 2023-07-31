796e225b5f05a267b03dd7a708df0abb8f0a44ec
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-07-31 21:57:27 +0600

    test addded

---
+++ test.js
@@ -0,0 +1,32 @@
+var productImages = document.getElementsByClassName("productImage");
+var modalImage = document.getElementById("modalImage");
+var modal = document.getElementById("myModal");
+
+for (var i = 0; i < productImages.length; i++) {
+  productImages[i].addEventListener("click", function () {
+    modalImage.src = this.src;
+    modal.style.display = "block";
+  });
+}
+
+document
+  .getElementsByClassName("close")[0]
+  .addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+window.addEventListener("scroll", function () {
+  modal.style.display = "none";
+});
+
+// Main code:
+
+window.addEventListener("scroll", function () {
+  modal.style.display = "none";
+});
