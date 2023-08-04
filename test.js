33dc3265354636b46a225ba7bb9ec2b090238a12
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-08-04 19:28:08 +0600

    clean up

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
 var productImages = document.getElementsByClassName("productImage");
 var modalImage = document.getElementById("modalImage");
 var modal = document.getElementById("myModal");
-
-for (var i = 0; i < productImages.length; i++) {}
